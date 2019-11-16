40d1d7d6033a70190d908444ccef8ce5e2cd233f
zak39 <fotia.baptiste@hotmail.com>
2019-11-16 12:12:35 +0100

    I created the first 'Database' classe file and it allows to centralize the interactions diffrents with the database

---
+++ lets-play_winform/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lets_play_winform
+{
+    class Database
+    {
+        // constructeur a vide
+        public Database() { }
+
+        // attributs
+        public string addrIPDB, usernameDB, passwordDB;
+
+        // Constructeur
+        public Database(string IP="127.0.0.1", string username="", string password="")
+        {
+            this.addrIPDB = IP;
+            this.usernameDB = username;
+            this.passwordDB = password;
+        }
+
+        // getters and setters
+        public string AddrIPDB
+        {
+            get => this.addrIPDB;
+            set => this.addrIPDB = value;
+        }
+
+        public string UsernameDB
+        {
+            get => this.usernameDB;
+            set => this.usernameDB = value;
+        }
+
+        public string PasswordDB
+        {
+            get => this.passwordDB;
+            set => this.passwordDB = value;
+        }
+
+        // Methodes
+    }
+}
