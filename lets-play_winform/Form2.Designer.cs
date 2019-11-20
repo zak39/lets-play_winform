ea579bbe0f7492d5bad23e588987c6a1992c8d12
zak39 <fotia.baptiste@hotmail.com>
2019-11-20 23:31:36 +0100

    I replaced the text name of 'button1' by 'Charger un fichier' and it load ONE file in the meantime

--- lets-play_winform/Form2.Designer.cs
+++ lets-play_winform/Form2.Designer.cs
@@ -95,7 +95,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 44);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Save Database";
+            this.button1.Text = "Charger un fichier";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
